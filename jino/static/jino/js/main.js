137467ae9297ba178d90143f84c820939545cd4d
SmerhsarP <SmerhsarP@gmail.com>
2019-04-18 00:28:22 +0500

    add POST request to server

--- jino/static/jino/js/main.js
+++ jino/static/jino/js/main.js
@@ -3,3 +3,19 @@ $(document).ready(function(){
 
   $('.collapsible').collapsible();
 });
+
+$(".collection-item").on("click", function () {
+  var result = $(this).text().substring(12);
+  var striptURL = "https://jino24.ru/clinic/valid";
+  console.log(result);
+
+  $.ajax({
+    url: scriptURL,
+    type: "POST",
+    dataType: "html",
+    data: ({name: result}),
+    success: function(response) {
+      console.log(response);
+    }
+  });
+});
