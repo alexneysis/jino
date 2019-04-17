9297ed8bbf0c083afa31287447ced474da29229e
Alexey <alexsis1998@gmail.com>
2019-04-17 22:35:51 +0300

    Fixed model in user

--- users/models.py
+++ users/models.py
@@ -6,8 +6,8 @@ class User(models.Model):
     class Meta:
         db_table = "users"
 
-    first_name = models.CharField(max_length=30)
     surname = models.CharField(max_length=30)
+    first_name = models.CharField(max_length=30)
     patronymic = models.CharField(max_length=30)
     phone = models.CharField(max_length=19)
     email = models.CharField(max_length=255)
